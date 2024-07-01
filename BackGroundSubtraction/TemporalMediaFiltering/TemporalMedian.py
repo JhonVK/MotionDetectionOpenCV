08721969be920f587abee1bfe6b587cc5d755d28
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-07-01 14:05:59 -0300

    update

--- BackGroundSubtraction/TemporalMediaFiltering/TemporalMedian.py
+++ BackGroundSubtraction/TemporalMediaFiltering/TemporalMedian.py
@@ -6,14 +6,13 @@ video_out='C:/Users/joaov/OneDrive/MotionDetectionOpenCV/Assets/videos/results/f
 
 cap=cv2.VideoCapture(video_source) #LEITURA DO VIDEO(CAPTURA)
 
-hasFrame, frame= cap.read() #HASFRAME VAI INDICAR SE TEM FRAME, FIRSTFRAME É O PRIMEIRO FRAME
-print(hasFrame, frame.shape)
+hasFrame, frame= cap.read() #hasFrame indica se o quadro foi lido com sucesso, frame contém o primeiro quadro
 
 fourcc= cv2.VideoWriter_fourcc(*'XVID') #GRAVAR O VIDEO DE RESULTADO fourcc é o codec (xvid é o formato para videos AVI)
 
 writer=cv2.VideoWriter(video_out, fourcc, 25, (frame.shape[1], frame.shape[0]), True ) #writer para gravar o video video_out vai ser onde vamos salvar, fourcc é o codec, 25 é o numero de frames p segundo, frame.shape é a dimensão do video. True significa que o video é colorido
 
-print(cap.get(cv2.CAP_PROP_FRAME_COUNT))#VAI MOSTRAR QUANTOS FRAMES A IMAGEM TEM
+#print(cap.get(cv2.CAP_PROP_FRAME_COUNT))#VAI MOSTRAR QUANTOS FRAMES A IMAGEM TEM
 #PRECISAMOS EXTRAIR 25 FRAMES DESSES 3000 (ALEATORIAMENTE)
 
 framesIds= cap.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=25) #GERANDO OS FRAMES
@@ -29,7 +28,7 @@ for fid in framesIds:
     hasFrame, frame= cap.read()
     frames.append(frame)
 
-print(np.asarray(frames).shape)
+#print(np.asarray(frames).shape)
 
 
 #calculando a mediana (pixel central) das imagens
@@ -38,3 +37,9 @@ medianFrame=np.median(frames, axis=0).astype(dtype=np.uint8)
 cv2.imshow('frame medio', medianFrame)
 cv2.waitKey(0)
 cv2.imwrite('model_median_frame', medianFrame)
+
+#Resetando o cap para iniciar no primeiro frame
+cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+#convertendo o median frame em tons de cinza (processamento se torna mais otimizado)
+
